334c48ade7bc0abe5655d27e6d457f79c0080362
Waty <wartaarnoutse@gmail.com>
2016-01-17 17:44:15 +0100

    Implemented ServerSided cache

--- generator/src/com/waty/server/calculate/EdgeFactory.java
+++ generator/src/com/waty/server/calculate/EdgeFactory.java
@@ -1,10 +1,13 @@
 package com.waty.server.calculate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Observer;
 
 public class EdgeFactory {
+    private static final HashMap<Integer, List<Edge>> CACHED_EDGES = new HashMap<>();
+
     public static void calculate(int level, Observer o) {
         KochFractal kf = new KochFractal();
         kf.setLevel(level);
@@ -15,8 +18,18 @@ public class EdgeFactory {
     }
 
     public static List<Edge> getAll(int level) {
+        synchronized (CACHED_EDGES) {
+            List<Edge> cachedEdges = EdgeFactory.CACHED_EDGES.get(level);
+            if (cachedEdges != null) return cachedEdges;
+        }
+
         List<Edge> edges = new ArrayList<>(getEdgeCount(level));
         calculate(level, (o, arg) -> edges.add((Edge) arg));
+
+        synchronized (CACHED_EDGES) {
+            CACHED_EDGES.put(level, edges);
+        }
+
         return edges;
     }
 
